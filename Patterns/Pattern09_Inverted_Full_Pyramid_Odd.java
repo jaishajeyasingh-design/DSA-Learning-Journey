2573dc894d11986576c1b0311949a464794b23f9
Jaisha J A <jaishajeyasingh@gmail.com>
2026-06-27 23:44:09 +0530

    Pattern 8 and 9 added successfully

---
+++ Patterns/Pattern09_Inverted_Full_Pyramid_Odd.java
@@ -0,0 +1,17 @@
+public class Pattern09_Inverted_Full_Pyramid_Odd {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
