07e2c2538a126bb67cca3cee67c27aa85d8a1c3a
Jaisha J A <jaishajeyasingh@gmail.com>
2026-06-27 22:59:35 +0530

    Added Pattern03 Number Half Pyramid

--- Patterns/Pattern01_Square.java
+++ Patterns/Pattern01_Square.java
@@ -3,8 +3,8 @@ public class Pattern01_Square {
 
         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
